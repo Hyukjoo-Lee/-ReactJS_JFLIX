31a194dc1f603adb99c6845c26a8a423e67ecb7a
Jaydon Lee <96518885+Hyukjoo-Lee@users.noreply.github.com>
2022-07-25 11:57:11 -0700

    adjusting path & deployment testing

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,7 +1,14 @@
 function Home() {
-    return <div style={{
-        backgroundColor:"white", height:"200vh"
-    }}></div>;
+  return (
+    <div
+      style={{
+        backgroundColor: "white",
+        height: "200vh",
+      }}
+    >
+      <h1>Home</h1>
+    </div>
+  );
 }
 
 export default Home;
