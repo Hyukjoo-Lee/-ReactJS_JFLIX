6402e9d7a3a31022a48804e8e7393d8ca84b9cdb
Jaydon Lee <96518885+Hyukjoo-Lee@users.noreply.github.com>
2022-07-26 14:14:47 -0700

    generated animation boxes

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -51,12 +51,62 @@ const Row = styled(motion.div)`
   width: 100%;
 `;
 
-const Box = styled(motion.div)`
+const Box = styled(motion.div)<{ bgPhoto: string }>`
   background-color: white;
   height: 200px;
+  background-image: url(${(props) => props.bgPhoto});
+  background-size: cover;
+  background-position: center center;
   color: red;
-  font-size: 65px;
+  font-size: 30px;
+
+  &:first-child {
+    transform-origin: center left;
+  }
+  &:last-child {
+    transform-origin: center right;
+  }
 `;
+
+const boxVariants = {
+  normal: {
+    scale: 1,
+  },
+
+  hover: {
+    scale: 1.2,
+    y: -30,
+    transition: {
+      delay: 0.2,
+      type: "tween",
+    },
+  },
+};
+
+const Info = styled(motion.div)`
+  padding: 10px;
+  background-color: ${(props) => props.theme.black.lighter};
+  opacity: 0;
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+  h4 {
+    text-align: center;
+    font-size: 10px;
+  }
+`;
+
+const infoVariants = {
+  hover: {
+    opacity: 1,
+    zIndex: 100,
+    transition: {
+      delay: 0.2,
+      type: "tween",
+    },
+  },
+};
+
 function Home() {
   const { data, isLoading } = useQuery<IGetMoviesResult>(
     ["movies", "now_playing"],
@@ -65,15 +115,25 @@ function Home() {
   const [leaving, setLeaving] = useState(false);
   const [index, setIndex] = useState(0);
   const windowWidth = useWindowDimensions();
+  const offset = 6;
 
   const increaseIndex = () => {
-    if (leaving) return;
-    setLeaving(true);
-    setIndex((prev) => prev + 1);
+    if (data) {
+      if (leaving) return;
+      setLeaving(true);
+      const totalMovies = data?.results.length - 1;
+      const maxIndex = Math.floor(totalMovies / offset) - 1;
+      setIndex((prev) => (prev === maxIndex ? 0 : prev + 1));
+    }
   };
+
   const toggleLeaving = () => {
     setLeaving((prev) => !prev);
   };
+
+  const onBoxClicked = (movieId:number) => {
+    
+  }
 
   return (
     <Wrapper>
@@ -83,10 +143,10 @@ function Home() {
         <>
           <Banner
             onClick={increaseIndex}
-            bgPhoto={makeImagePath(data?.results[3].backdrop_path || "")}
+            bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}
           >
-            <Title>{data?.results[3].title}</Title>
-            <Overview>{data?.results[3].overview}</Overview>
+            <Title>{data?.results[0].title}</Title>
+            <Overview>{data?.results[0].overview}</Overview>
           </Banner>
           <Slider>
             <AnimatePresence initial={false} onExitComplete={toggleLeaving}>
@@ -97,9 +157,27 @@ function Home() {
                 transition={{ types: "tween", duration: 1 }}
                 key={index}
               >
-                {[1, 2, 3, 4, 5, 6].map((i) => (
-                  <Box key={i}>{i}</Box>
-                ))}
+                {data?.results
+                  .slice(1)
+                  .slice(offset * index, offset * index + offset)
+                  .map((movie) => (
+                    <Box
+                      key={movie.id}
+                      whileHover="hover"
+                      initial="normal"
+                      variants={boxVariants}
+                      transition={{ type: "tween" }}
+                      onClick={() => onBoxClicked(movie.id)}
+                      bgPhoto={makeImagePath(
+                        movie?.backdrop_path || movie?.poster_path,
+                        "w400"
+                      )}
+                    >
+                      <Info variants={infoVariants}>
+                        <h4>{movie.title}</h4>
+                      </Info>
+                    </Box>
+                  ))}
               </Row>
             </AnimatePresence>
           </Slider>
